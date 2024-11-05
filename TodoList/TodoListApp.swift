b60ad9c70126cfe5b5ebf7503889daa354bb02fd
Monisankar <mxn170301@darden.com>
2024-11-05 13:20:23 +0530

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Monisankar Nath on 05/11/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
